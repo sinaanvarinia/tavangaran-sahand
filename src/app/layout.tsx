9670da67c9f3d1c6cbb5e5b6410d8bed90c2d291
sina <sina.anvarinia@gmail.com>
2023-10-29 10:27:58 +0330

    favicon added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
-import type { Metadata } from 'next'
-import './globals.css'
- 
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
   title: "گروه صنعتی توانگران سهند",
   description: "created by sina anvarinia",
+  // icons: {
+  //   icon: "/public/favicon/android-chrome-192x192.png",
+  //   apple: "/public/favicon/apple-touch-icon.png",
+  // },
 };
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="fa" dir='rtl'>
+    <html lang="fa" dir="rtl">
+   
       <body className="">{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 import { Hero } from '@/components/hero/Hero'
 import Navbar from '@/components/navbar/Navbar'
-import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="flex flex-col items-center">
-      <Navbar />
-      <Hero/>
     
+      <Navbar />
+      <Hero />
     </main>
-  )
+  );
 }
