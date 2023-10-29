40505cfd270b9dcb106f48837b6c0a1e83d96a07
sina <sina.anvarinia@gmail.com>
2023-10-29 12:50:40 +0330

    favicon fixed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from "next";
 import "./globals.css";
+// import Head from "next/head";
 
 export const metadata: Metadata = {
   title: "گروه صنعتی توانگران سهند",
   description: "created by sina anvarinia",
   // icons: {
-  //   icon: "/public/favicon/android-chrome-192x192.png",
-  //   apple: "/public/favicon/apple-touch-icon.png",
+  //   icon: "public/favIcon/android-chrome-192x192.png",
+  //   apple: "public/favIcon/apple-icon.png",
   // },
 };
 
@@ -17,7 +18,29 @@ export default function RootLayout({
 }) {
   return (
     <html lang="fa" dir="rtl">
-   
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="theme-color" content="#ffffff"></meta>
+      </head>
       <body className="">{children}</body>
     </html>
   );
