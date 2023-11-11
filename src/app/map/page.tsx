485a62bc8551ad4a04ed1f639b6e0ad6c7885057
sina <sina.anvarinia@gmail.com>
2023-11-11 11:44:00 +0330

    map added from react-leaflet

---
+++ src/app/map/page.tsx
@@ -0,0 +1,31 @@
+"use client";
+
+import React from "react";
+import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer, useMap } from "react-leaflet";
+import Head from "next/head";
+
+const Map = () => {
+  const position = [38.058, 46.209];
+
+  return (
+    <MapContainer
+      className={styles.map}
+      center={position}
+      zoom={13}
+      scrollWheelZoom={false}
+    >
+      <TileLayer
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      />
+      <Marker position={position}>
+        <Popup>
+          شرکت توانگران سهند <br />
+        </Popup>
+      </Marker>
+    </MapContainer>
+  );
+};
+
+export default Map;
--- src/components/address/Address.tsx
+++ src/components/address/Address.tsx
@@ -2,13 +2,14 @@
 import React from "react";
 import { contactUs } from "@/constants";
 import Image from "next/image";
+import Map from "../map/Map";
 
 const Address = () => {
   return (
     <section className=" w-full">
       <div className="container  flex flex-col gap-4 text-center md:flex-row mx-auto">
         {/* Address */}
-        <div className="md:w-1/2 p-12 text-slate-500 shadow-md my-12 rounded-3xl">
+        <div className="md:w-1/2 h-[360px] p-12 text-slate-500 shadow-md my-12 rounded-3xl">
           <h3 className="text-xl mb-5">آدرس دفتر مرکزی:</h3>
           <p className="leading-9">
             {" "}
@@ -24,18 +25,16 @@ const Address = () => {
                     <item.icon />
                   </span>
                   <span className="max-md:hidden">{item.title}</span>
-                  <span className=" text-slate-600">
-                    {" "}
-                    {item.value}
-                  </span>
+                  <span className=" text-slate-600"> {item.value}</span>
                 </li>
               ))}
             </div>
           </p>
         </div>
         {/* map */}
-        <div className="md:w-1/2 justify-center items-center my-12">
-          <Image src="/map.png" alt="map" width={700} height={500} />
+        <div className="md:w-1/2 rounded-3xl shadow-md sm:md:w-full h-[360px] justify-center items-center md:my-12 mb-12">
+          {/* <Image src="/map.png" alt="map" width={700} height={500} /> */}
+          <Map />
         </div>
       </div>
     </section>
