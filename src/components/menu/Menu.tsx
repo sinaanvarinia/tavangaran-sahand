529bcab622e0a93af667311ebf20ad5148263aa7
sina <sina.anvarinia@gmail.com>
2023-11-18 14:07:24 +0330

    navbar and menu modified

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -27,7 +27,7 @@ const Menu = () => {
         </i>
       )}
       {open && (
-        <div className="focus:bg-yellow-400 bg-gradient-to-b from-[#8d804c] via-[#dfb307] via-30% to-[#9a7c06] z-10 text-slate-100 flex flex-col items-center gap-1 absolute w-full h-[calc(90vh)] right-0 top-[51px]">
+        <div className="focus:bg-yellow-400 bg-gradient-to-b from-[#8d804c] via-[#dfb307] via-50% to-[#9a7c06] z-10 text-slate-100 flex flex-col items-center gap-1 absolute w-full h-[calc(90vh)] right-0 top-[51px]">
           {navbarItems.map((item) => (
             <li
               onClick={() => setOpen(false)}
