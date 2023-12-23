f8d1d0335ff2a2dba7d15a97ff9f464ca24fcf4f
sina <sina.anvarinia@gmail.com>
2023-12-23 14:12:18 +0330

    base path modified

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   output: "export",
   images: { unoptimized: true },
 
-  basePath: "https://sinaanvarinia.github.io/tavangaran-sahand/",
+  basePath: "/sinaanvarinia.github.io/tavangaran-sahand/",
 };
 
 module.exports = nextConfig;
