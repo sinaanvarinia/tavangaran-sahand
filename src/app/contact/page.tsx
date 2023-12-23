4d27dcf5dfca3824742f6c055dd3076d66381e84
sina <sina.anvarinia@gmail.com>
2023-12-23 11:24:39 +0330

    contact page made empty

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,16 +1,16 @@
-"use client"
+"use client";
 import Address from "@/components/address/Address";
 import React from "react";
 
-const contactUsPage = () => {
+const contact = () => {
   return (
     <div className="flex min-h-screen  md:p-20 p-5  bg-cover flex-col   bg-[url('/globe.jpg')]">
       <div className="container mx-auto flex flex-col items-end opacity-90">
-        <Address />
+        {/* <Address /> */}
         {/* <Map /> */}
       </div>
     </div>
   );
 };
 
-export default contactUsPage;
+export default contact;
