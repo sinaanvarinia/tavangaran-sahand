c132017fbffe1ab79a78ad0657911f0409bd6fd1
sina <sina.anvarinia@gmail.com>
2023-12-24 09:59:22 +0330

    map without marker working on vercel

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,10 +1,13 @@
 "use client";
 import Address from "@/components/address/Address";
 import Map from "@/components/map/Map";
-import React from "react";
+import React, { useEffect } from "react";
 
-const contact = () => {
+const Contact = () => {
+
+ 
   return (
+    
     <div className="flex min-h-screen  md:p-20 p-5  bg-cover flex-col   bg-[url('/globe.jpg')]">
       <div className="container mx-auto flex flex-col items-end opacity-90">
         <Address />
@@ -14,4 +17,4 @@ const contact = () => {
   );
 };
 
-export default contact;
+export default Contact;
