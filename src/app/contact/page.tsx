7a3fa8b8dff4bca47af05f7842b3ead496985975
sina <sina.anvarinia@gmail.com>
2023-12-24 09:36:08 +0330

    address and map added to working site on vercel

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 import Address from "@/components/address/Address";
+import Map from "@/components/map/Map";
 import React from "react";
 
 const contact = () => {
   return (
     <div className="flex min-h-screen  md:p-20 p-5  bg-cover flex-col   bg-[url('/globe.jpg')]">
       <div className="container mx-auto flex flex-col items-end opacity-90">
-        {/* <Address /> */}
-        {/* <Map /> */}
+        <Address />
+        <Map />
       </div>
     </div>
   );
