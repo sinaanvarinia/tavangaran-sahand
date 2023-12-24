4c5f44e64d07c8a662265c19fd3c148e568acbfe
sina <sina.anvarinia@gmail.com>
2023-12-24 09:29:29 +0330

    2

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: { unoptimized: true },
+//   output: "export",
+//   images: { unoptimized: true },
 
-  basePath:
-    "sinaanvarinia.github.io/tavangaran-sahand",
+//   basePath:
+//     "/sinaanvarinia.github.io/tavangaran-sahand",
 };
 
 module.exports = nextConfig;
