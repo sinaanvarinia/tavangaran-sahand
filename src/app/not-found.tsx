aae8917a745997071cdcc0ab4ed4bf51f929dce0
sina <sina.anvarinia@gmail.com>
2023-12-25 10:45:49 +0330

    pics compressed and 404 page changed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,20 +3,28 @@ import React from "react";
 
 const NotFound = () => {
   return (
-    <div className="container flex-col gap-10 m-auto justify-center flex items-center w-full h-screen ">
-      <Image width={200} height={100} className="bg-slate-700 p-1 rounded-xl shadow-xl object-contain" src="/logo.svg" alt="logo" />
-
-      <h1 className="text-center text-3xl  mx-auto text-slate-500">
-        صفحه مورد نظر پیدا نشد.
-      </h1>
+    <div className="container flex-col gap-10 m-auto justify-center flex w-full h-screen ">
       <div className="w-400  h-300 ">
         <Image
           alt="notfound page"
-          src="/notFound.gif"
+          src="/not_found.svg"
           className="rounded-xl shadow-xl"
           width={800}
           height={300}
         />
+      </div>
+      <div className="flex flex-row-reverse items-center justify-between">
+        <Image
+          width={100}
+          height={100}
+          className="bg-slate-300 p-1 rounded-xl shadow-xl object-contain"
+          src="/logo.svg"
+          alt="logo"
+        />
+
+        <h1 className=" text-xl text-slate-500">
+          صفحه مورد نظر پیدا نشد.
+        </h1>
       </div>
     </div>
   );
